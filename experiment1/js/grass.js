0a291edc76921471b420d1f5b0514d5ea0b0f8ac
Ellie <151094173+EllieM-gd@users.noreply.github.com>
2025-06-11 20:34:59 -0700

    Fixed console log spam

--- experiment1/js/grass.js
+++ experiment1/js/grass.js
@@ -1,7 +1,6 @@
 function draw_grass() {
     let col_lerp_factor = document.getElementById("offsetSlider")
     col_lerp_factor = col_lerp_factor.value/col_lerp_factor.max
-    console.log(col_lerp_factor)
     
     let mult = lerpColor( color(11, 24, 107), color(249,168,53),col_lerp_factor)
     let primary = $('#grassColorPicker').val();
